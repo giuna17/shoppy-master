52a65dcf157e2539e4e5ff0464a163d34a99d3d2
giuna17 <pep6@list.ru>
2025-05-24 08:27:34 +0400

    fix: update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/shoppy/' : '/',
+  base: mode === 'production' ? '/nekos-shop/' : '/',
   server: {
     host: "::",
     port: 8080,
