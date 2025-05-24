ed368cf24a0fb4b2c009ed94fa6e120f0e95b9e7
giuna17 <pep6@list.ru>
2025-05-24 10:28:59 +0400

    fix: update configs for Vercel deployment and fix routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
-  
+  const base = isProduction ? '/shoppy-master/' : '/';
+
   return {
-    base: isProduction ? '/shoppy-master/' : '/',
+    base,
     server: {
-      host: "::",
+      host: '::',
       port: 8080,
     },
     plugins: [react()],
     resolve: {
       alias: {
-        "@": resolve(__dirname, "./src"),
+        '@': resolve(__dirname, './src'),
       },
     },
     build: {
@@ -25,17 +26,17 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
       rollupOptions: {
         input: {
-          main: resolve(__dirname, 'index.html')
+          main: resolve(__dirname, 'index.html'),
         },
         output: {
-          entryFileNames: `assets/[name].[hash].js`,
-          chunkFileNames: `assets/[name].[hash].js`,
-          assetFileNames: `assets/[name].[hash].[ext]`
-        }
-      }
+          entryFileNames: 'assets/[name]-[hash].js',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
     },
     define: {
-      'process.env.NODE_ENV': `"${mode}"`
-    }
+      'process.env.NODE_ENV': `"${mode}"`,
+    },
   };
 });
